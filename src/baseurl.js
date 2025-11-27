6418fe2079e436cd2c0ebba2212df6b53ca9334c
anujkumarchoudhary <anujkumarchoudhary6699@gmail.com>
2025-11-27 23:03:43 +0530

    git init

--- src/baseurl.js
+++
@@ -1,2 +0,0 @@
-// export const baseUrl = "https://gauto-backend.vercel.app/api"
-export const baseUrl = "http://localhost:4000/api";
--- src/components/ui/InputField.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const InputField = ({ type, name, value,handleChange,maxLength, placeholder, className }) => {
-    return (
-        <input type={type} name={name} value={value} onChange={handleChange} maxLength={maxLength} placeholder={placeholder} className={`${className} bg-white outline-none border border-slate-300 px-4 py-2 my-1 w-full`} />
-    )
-}
-
-export default InputField
